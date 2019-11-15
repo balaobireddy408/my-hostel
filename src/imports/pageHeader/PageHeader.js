52bebc24c9ec1a803cf6d53f51e48d935929dd2b
balaobireddy408 <balaobireddy408@gmail.com>
2019-11-15 23:01:51 +0530

    layout step1

---
+++ src/imports/pageHeader/PageHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Box } from "./styledComponets";
+
+const PageHeader = () => {
+  return (
+    <Box>
+      <a href="#">home</a>
+      <a href="#">contact us</a>
+      <a href="#">about us</a>
+    </Box>
+  );
+};
+
+export default PageHeader;
---
+++ src/imports/pageHeader/styledComponets.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledLink = styled.a`
+  color: palevioletred;
+  font-weight: bold;
+`;
+
+const Box = styled.div`
+  display: flex;
+  height: 70px;
+  width: 100%;
+  background-color: #edeff8;
+`;
+
+export { StyledLink, Box };
