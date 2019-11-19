167686cdbb4afebb101f5dadff3755e85e9d77aa
balaobireddy408 <balaobireddy408@gmail.com>
2019-11-19 22:43:22 +0530

    background color added

---
+++ src/imports/pages/contactUs/styledComponents.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Section = styled.div`
+  width: 100%;
+  height: 250px;
+  text-align: center;
+  background-color: #f3f3f7;
+`;
+
+export { Section };
